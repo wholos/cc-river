a0d44162a6e9eceb141358345c73bce36bf50af9
holos <holosnikitatt@gmail.com>
2025-12-10 11:20:11 +0200

    Delete as.c

--- as.c
+++
@@ -1,9 +0,0 @@
-// Autostart
-#include <stdlib.h>
-
-int main() {
-  system("pipewire");
-  system("pipewire-pulse");
-  system("wireplumber");
-  system("wlr-randr --output HDMI-A-1 --mode 1366x768");
-}
